ee60b9679c24536743dcf245c9ffb1474bb59f04
Kevin Sawicki <kevin@github.com>
2011-08-26 00:42:41 -0700

    Add operation for checking out a specific tree

---
+++ plugin/src/main/java/org/jenkinsci/git/TreeCheckoutOperation.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2011 GitHub Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+package org.jenkinsci.git;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.dircache.DirCache;
+import org.eclipse.jgit.dircache.DirCacheCheckout;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+
+import hudson.FilePath.FileCallable;
+import hudson.remoting.VirtualChannel;
+
+/**
+ * Operation that checks out a specific commit
+ *
+ * @author Kevin Sawicki (kevin@github.com)
+ */
+public class TreeCheckoutOperation implements FileCallable<ObjectId> {
+
+	/** serialVersionUID */
+	private static final long serialVersionUID = 1247715173832080149L;
+
+	private final Repository repo;
+
+	private final RevCommit commit;
+
+	/**
+	 * Create tree checkout operation
+	 *
+	 * @param repo
+	 *            must be non-null
+	 * @param commit
+	 *            must be non-null
+	 */
+	public TreeCheckoutOperation(Repository repo, RevCommit commit) {
+		if (repo == null)
+			throw new IllegalArgumentException("Repo cannot be null");
+		if (commit == null)
+			throw new IllegalArgumentException("Commit cannot be null");
+		this.repo = repo;
+		this.commit = commit;
+	}
+
+	public ObjectId invoke(File file, VirtualChannel channel)
+			throws IOException {
+		RevTree tree = commit.getTree();
+		DirCache dirCache = repo.lockDirCache();
+		DirCacheCheckout co = new DirCacheCheckout(repo, dirCache, tree);
+		co.setFailOnConflict(false);
+		if (!co.checkout())
+			throw new IOException("Checkout failed");
+		return tree;
+	}
+}
---
+++ plugin/src/test/java/org/jenkinsci/git/TreeCheckoutOperationTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2011 GitHub Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+package org.jenkinsci.git;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.junit.Test;
+
+/**
+ * Unit tests of {@link TreeCheckoutOperation}
+ *
+ * @author Kevin Sawicki (kevin@github.com)
+ */
+public class TreeCheckoutOperationTest extends GitTestCase {
+
+	/**
+	 * Create operation with null repository
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void nullRepository() {
+		new TreeCheckoutOperation(null, null);
+	}
+
+	/**
+	 * Create operation with null commit
+	 *
+	 * @throws IOException
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void nullCommit() throws IOException {
+		new TreeCheckoutOperation(git.repo(), null);
+	}
+
+	/**
+	 * Test checking out a tree from previous commit
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void checkoutPreviousCommitTree() throws Exception {
+		RevCommit commit = git.add("file.txt", "a");
+		git.add("file.txt", "b");
+		Repository repo = git.repo();
+		File file = new File(repo.getWorkTree(), "file.txt");
+		FileReader reader = new FileReader(file);
+		assertEquals('b', (char) reader.read());
+		reader.close();
+		TreeCheckoutOperation op = new TreeCheckoutOperation(repo, commit);
+		ObjectId tree = op.invoke(null, null);
+		assertEquals(commit.getTree(), tree);
+		reader = new FileReader(file);
+		assertEquals('a', (char) reader.read());
+		reader.close();
+	}
+}
